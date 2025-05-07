53c42b12aa2a91c95eec22d97db2912fea22f0ad
Beymamat Rakhmatov <beymamatrakhmatov@MacBook-Pro-Beymamat.local>
2025-05-08 01:39:34 +0500

    init

---
+++ news_iosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news_iosApp
+//
+//  Created by Beymamat Rakhmatov on 08/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
